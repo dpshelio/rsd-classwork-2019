25f8da9a2056c77b20bfc575ef9dd754f9d56eef
David Perez-Suarez <d.perez-suarez@ucl.ac.uk>
2019-10-31 15:38:50 +0000

    Mulitple ranges and bug fixed
    UCL-RITS/rsd-classwork-2019#30

--- 05-testing/test_times.py
+++ 05-testing/test_times.py
@@ -1,4 +1,7 @@
+import datetime
+
 import times
+
 
 def test_given_input():
     large = times.time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
@@ -12,6 +15,14 @@ def test_class_time():
     short = times.time_range("2019-10-31 10:05:00", "2019-10-31 12:55:00", 3, 600)
     result =  times.overlap_time(large, short)
     assert result == short
+
+def test_20_min():
+    large = times.time_range("2019-01-01 00:00:00", "2019-01-01 23:50:00", 24, 10 * 60)
+    short = times.time_range("2019-01-01 00:30:00", "2019-01-01 23:55:00", 24, 35 * 60)
+    result = times.overlap_time(large, short)
+    assert all([(datetime.datetime.strptime(t1, "%Y-%m-%d %H:%M:%S") - 
+    datetime.datetime.strptime(t0, "%Y-%m-%d %H:%M:%S")).total_seconds() == 20 * 60
+    for t0, t1 in result])
 
 # What happens when...
 
