2fc7d39628193d57a5c97570efca0a48b570ba18
David Perez-Suarez <d.perez-suarez@ucl.ac.uk>
2019-10-31 11:35:19 +0000

    Adds a test for class time
    UCL-RITS/rsd-classwork-2019#25

---
+++ 05-testing/test_times.py
@@ -0,0 +1,18 @@
+import times
+
+def test_given_input():
+    large = times.time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
+    short = times.time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60)
+    result = times.overlap_time(large, short)
+    expected = [('2010-01-12 10:30:00', '2010-01-12 10:37:00'), ('2010-01-12 10:38:00', '2010-01-12 10:45:00')]
+    assert result == expected
+
+def test_class_time():
+    large = times.time_range("2019-10-31 10:00:00", "2019-10-31 13:00:00")
+    short = times.time_range("2019-10-31 10:05:00", "2019-10-31 12:55:00", 3, 600)
+    result =  times.overlap_time(large, short)
+    assert result == short
+
+# What happens when...
+
+# ...
