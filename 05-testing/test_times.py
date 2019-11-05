43b17510dd8177153b138307443ba7e5aac83f17
David Perez-Suarez <d.perez-suarez@ucl.ac.uk>
2019-11-05 21:12:15 +0000

    Adds a fixture to use a yaml file as input to tests
    Answers UCL-RITS/rsd-classwork-2019#34

--- 05-testing/test_times.py
+++ 05-testing/test_times.py
@@ -1,26 +1,34 @@
 import datetime
 
 import pytest
+import yaml
 
 import times
 
 
-@pytest.mark.parametrize('test_input, expected', [
-    #(input ranges, expected ranges)
-    # given input
-    ([["2010-01-12 10:00:00", "2010-01-12 12:00:00"], ["2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60]], [('2010-01-12 10:30:00', '2010-01-12 10:37:00'), ('2010-01-12 10:38:00', '2010-01-12 10:45:00')]),
-    # class time
-    ([["2019-10-31 10:00:00", "2019-10-31 13:00:00"], ["2019-10-31 10:05:00", "2019-10-31 12:55:00", 3, 600]], times.time_range("2019-10-31 10:05:00", "2019-10-31 12:55:00", 3, 600)),
-    # no overlap
-    ([["2019-01-01 00:00:00", "2019-01-01 23:50:00"], ["2019-01-02 00:30:00", "2019-01-02 23:55:00"]], []),
-    # touching edges
-    ([["2019-10-31 00:00:00", "2019-10-31 00:50:00", 3, 600], ["2019-10-31 00:10:00", "2019-10-31 01:00:00", 3, 600]], []),
-])
-def test_many(test_input, expected):
-    large = times.time_range(*test_input[0])
-    short = times.time_range(*test_input[1])
+with open("fixture.yaml", 'r') as yamlfile:
+    fixture = yaml.safe_load(yamlfile)
+
+
+@pytest.mark.parametrize('test_name', fixture) # fixture is a list of dictionaries [{'given':...}, {'class':...}, ]
+def test_many(test_name):
+    # test_name is a dictionary like: {'name': {'test_input':..., 'expected':...}
+    properties = list(test_name.values())[0] # We know there's only one element inside 'name'
+    # get the input and expected values for each test
+    test_input = properties['test_input']
+    expected = properties['expected']
+    large = times.time_range(*test_input['interval_1'])
+    short = times.time_range(*test_input['interval_2'])
     result = times.overlap_time(large, short)
-    assert result == expected
+    if isinstance(expected, list):
+        # If expected is a list when we've writen the results on the yaml
+        if expected:
+            # If it's not an empty list we need to convert a list of lists into a list of tuples.
+            expected = [(start, stop) for start, stop in expected]
+        assert result == expected
+    else:
+        # If it's not a list, then `expected` is the name of one of the inputs
+        assert result == times.time_range(*test_input[expected])
 
 def test_20_min():
     large = times.time_range("2019-01-01 00:00:00", "2019-01-01 23:50:00", 24, 10 * 60)
